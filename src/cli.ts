27ac2bce6b2885298a261b2d5bebe0802008202b
kendavidson <ken.j.davidson@live.ca>
2022-11-20 12:42:48 -0500

    chore(version): get package version workin

--- src/cli.ts
+++ src/cli.ts
@@ -5,10 +5,25 @@ import ApiCommand from './api/ApiCommand.js';
 import LeaguesCommand from './league/LeaguesCommand.js';
 import ConfigCommand from './oauth2/ConfigCommand.js';
 import LoginCommand from './oauth2/LoginCommand.js';
+import { readFileSync } from 'fs';
+
+export const getProjectRoot = () => {
+  let result = new URL(import.meta.url);
+  let pathname = result.pathname;
+  let pathArray = pathname.split('/');
+  let filename = pathArray.pop();
+  let distpath = pathArray.pop();
+  let dirname = pathArray.join('/');
+
+  return { pathname, dirname, distpath, filename };
+};
+
+const { dirname } = getProjectRoot();
+const pkg: any = JSON.parse(readFileSync(`${dirname}/package.json`, 'utf-8'));
 
 const cli = new Command('yfs')
-  .description('1.0.0')
-  .version('Manage Yahoo! Fantasy teams from the command line')
+  .description(pkg.description)
+  .version(`v${pkg.version}`)
   .addCommand(ConfigCommand)
   .addCommand(LoginCommand)
   .addCommand(ApiCommand)
